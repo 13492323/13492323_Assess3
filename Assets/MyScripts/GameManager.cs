37299770509007972a845847b88e63bb17ae5ee2
13492323 <xueyan.yang-1@student.uts.edu.au>
2023-09-15 22:19:13 +1000

    111

---
+++ Assets/MyScripts/GameManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public List<AudioClip> BGMClipList;
+    public List<AudioClip> EffectClipList;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
